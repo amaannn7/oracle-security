362c30b9675c5caf3e5c7011df1c17397fcc1248
amaannn7 <tigo.yusuf@gmail.com>
2026-02-03 10:27:20 +0530

    added some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ScrollToTop from "./components/ScrollToTop";
 import ScrollToTopButton from "./components/ScrollToTopButton";
+import PageTransition from "./components/PageTransition";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +37,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ScrollToTop />
-        {children}
+        <PageTransition>
+          {children}
+        </PageTransition>
         <ScrollToTopButton />
       </body>
     </html>
